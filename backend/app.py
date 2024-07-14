159eb430343dd6e31d0fc0a4dd1b9ceafa31c506
Gere_X <gergely.gere@gmail.com>
2024-07-14 15:43:37 +0200

    working but list null

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+CORS(app) 
 
 @app.route('/api')
 def get_data():
@@ -12,13 +14,14 @@ def get_data():
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
         data = []
-        for item in soup.find_all('div', class_='dates-info ng-tns-c180-1'):
-            title = item.find('h2').text
+        for item in soup.find_all('div', class_='col info-div'):
+            title = item.find('h5').text
             price = item.find('span', class_='price').text
             data.append({'title': title, 'price': price})
         return jsonify(data)
     else:
         return jsonify({'error': 'Failed to retrieve the page'}), response.status_code
+
 
 if __name__ == '__main__':
     app.run(debug=True)
