d4a96dfec0d0ee379dfb0ec289823c47226021ff
Gere_X <gergely.gere@gmail.com>
2024-07-14 17:59:28 +0200

    idemo 3

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
 
 app = Flask(__name__)
 CORS(app)
@@ -21,9 +22,9 @@ def get_data():
         driver.get(url)
         # Adatok gyűjtése Selenium segítségével
         data = []
-        elements = driver.find_element(By.CLASS_NAME, 'header-title')
-       
-        print(elements)
+        input = driver.find_element(By.ID, 'duration')
+        input.send_keys(3)
+        time.sleep(10)
            
         return jsonify(data)
 
