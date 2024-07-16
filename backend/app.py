919390aade9fd3d2c7b81f35ce6ceaee87a7efcb
Gere_X <gergely.gere@gmail.com>
2024-07-16 14:02:37 +0200

    prhuzamos

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,15 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import asyncio
+import concurrent.futures
+import logging
 from ddor_travel import get_travel_price_ddor
 from sava_travel import get_travel_price_sava
 
 app = Flask(__name__)
 CORS(app)
 
-
-
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
 @app.route('/api', methods=['POST'])
 def get_data():
@@ -18,19 +19,25 @@ def get_data():
     ages = data.get('ages')
     sport = data.get('sport')
 
+    response_data = {}
+    
     try:
-        sava_price =  get_travel_price_sava(arrival_date, departure_date, ages)
-        print(sava_price)
-        ddor_price =  get_travel_price_ddor(arrival_date, departure_date, ages, sport)
-        print(ddor_price)
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            future_sava = executor.submit(get_travel_price_sava, arrival_date, departure_date, ages)
+            future_ddor = executor.submit(get_travel_price_ddor, arrival_date, departure_date, ages, sport)
+
+        sava_price = future_sava.result()
+        ddor_price = future_ddor.result()
+            
         response_data = {
             'ddor_price': ddor_price,
             'sava_price': sava_price
         }
-        print(response_data)
-        
+        app.logger.info(f"Response data: {response_data}")
+            
     except Exception as e:
         response_data = {'error': str(e)}
+        app.logger.error(f"Error: {e}")
 
     return jsonify(response_data)
 
